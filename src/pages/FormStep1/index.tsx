133d8e00ba96993dee26de96d87e6c86a235ffab
Icaro Peretti <icaroperetti50@gmail.com>
2021-10-13 11:08:02 -0300

    feat:stylizing FormStep1 and navigating to next page

--- src/pages/FormStep1/index.tsx
+++ src/pages/FormStep1/index.tsx
@@ -1,10 +1,30 @@
+import { useHistory } from "react-router-dom";
 import * as C from "./styles";
 import { Theme } from "../../components/Theme";
 
 export const FormStep1 = () => {
+  const history = useHistory();
+
+  const handleNextStep = () => {
+    history.push("/step2");
+  };
+
   return (
     <Theme>
-      <C.Container>Passo 1</C.Container>
+      <C.Container>
+        <p>Step 1/3</p>
+        <h1>Let's start with your name</h1>
+        <p>Fill the field below with your name.</p>
+
+        <hr />
+
+        <label>
+          Your name:
+          <input type="text" autoFocus />
+        </label>
+
+        <button onClick={handleNextStep}>Next</button>
+      </C.Container>
     </Theme>
   );
 };
