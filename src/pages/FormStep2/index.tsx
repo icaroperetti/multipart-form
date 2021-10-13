02e5077f6948bf63d793705092e3c9e3807165d7
Icaro Peretti <icaroperetti50@gmail.com>
2021-10-13 12:33:22 -0300

    feat: Creating select of programming level

--- src/pages/FormStep2/index.tsx
+++ src/pages/FormStep2/index.tsx
@@ -1,10 +1,61 @@
-import * as C from './styles';
-
+import { useHistory } from "react-router-dom";
+import * as C from "./styles";
+import { useForm, FormActions } from "../../contexts/FormContext";
+import { Theme } from "../../components/Theme";
+import { SelectOption } from "../../components/SelectOption";
+import { ChangeEvent, useEffect } from "react";
 
 export const FormStep2 = () => {
+  const history = useHistory();
+  const { state, dispatch } = useForm();
+
+  useEffect(() => {
+    dispatch({
+      type: FormActions.setCurrentStep,
+      payload: 2,
+    });
+  }, []);
+
+  const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    dispatch({
+      type: FormActions.setName,
+      payload: e.target.value,
+    });
+  };
+
+  const handleNextStep = () => {
+    if (state.name !== "") {
+      history.push("/step2");
+    } else {
+      alert("Please enter your name");
+    }
+  };
+
   return (
-    <C.Container>
-      Passo 2
-    </C.Container>
+    <Theme>
+      <C.Container>
+        <p>Step 2/3</p>
+        <h1>Let's start with your name</h1>
+        <p>Fill the field below with your name.</p>
+
+        <hr />
+
+        <SelectOption
+          title="Beginner"
+          description="Programming less than 2 years"
+          icon="🥳"
+          selected={state.level === 0}
+        />
+
+        <SelectOption
+          title="I'm programmer"
+          description="Programming more than 2 years"
+          icon="😎"
+          selected={state.level === 1}
+        />
+
+        <button onClick={handleNextStep}>Next</button>
+      </C.Container>
+    </Theme>
   );
-}
+};
